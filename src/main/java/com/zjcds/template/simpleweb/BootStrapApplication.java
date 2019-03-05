037f42110ad6f92c41ea0625d4f146c5eeb9ed7a
niezg <niezg@zjcds.com>
2019-03-05 17:49:29 +0800

    enable jpa

--- src/main/java/com/zjcds/template/simpleweb/BootStrapApplication.java
+++ src/main/java/com/zjcds/template/simpleweb/BootStrapApplication.java
@@ -1,11 +1,16 @@
 package com.zjcds.template.simpleweb;
 
+import com.zjcds.common.jpa.impl.CustomRepostoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableJpaRepositories(basePackages={"com.zjcds"},repositoryBaseClass= CustomRepostoryImpl.class)
+@EntityScan("com.zjcds")
 public class BootStrapApplication {
 
 	public static void main(String[] args) {
